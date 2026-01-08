c4d4497c32fbaa7f6a2c9ae6da424f94c6c855f5
Coen Jacobs <coenjacobs@gmail.com>
2026-01-08 15:43:09 +0100

    Move main compose logic to separate command class

---
+++ src/Commands/Compose.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace CoenJacobs\Mozart\Commands;
+
+use CoenJacobs\Mozart\Mover;
+use CoenJacobs\Mozart\PackageFactory;
+use CoenJacobs\Mozart\PackageFinder;
+use CoenJacobs\Mozart\Replacer;
+use Exception;
+
+class Compose
+{
+    private string $workingDir;
+
+    public function __construct(string $workingDir)
+    {
+        $this->workingDir = $workingDir;
+    }
+
+    /**
+     * Main logic of the moving and replacing of the files, that form the basic
+     * functionality of Mozart. Finds and loads the main package, from which
+     * the Mozart configuration is read. Then the dependencies that need to be
+     * transformed by Mozart are detected and sent to the respective handlers.
+     */
+    public function execute(): void
+    {
+        $composerFile = $this->workingDir . DIRECTORY_SEPARATOR . 'composer.json';
+
+        try {
+            $factory = new PackageFactory();
+            $package = $factory->createPackage($composerFile);
+        } catch (Exception $e) {
+            throw $e;
+        }
+
+        if (! $package->isValidMozartConfig() || empty($package->getExtra())) {
+            throw new Exception('Mozart config not readable in composer.json at extra->mozart');
+        }
+
+        $config = $package->getExtra()->getMozart();
+
+        if (empty($config)) {
+            throw new Exception('Mozart config not readable in composer.json at extra->mozart');
+        }
+
+        $config->setWorkingDir($this->workingDir);
+
+        $require = $config->getPackages();
+        if (empty($require)) {
+            $require = $package->getRequire();
+        }
+
+        $finder = new PackageFinder();
+        $finder->setConfig($config);
+
+        $package->loadDependencies($finder);
+        $packages = $finder->findPackages($package->getDependencies());
+
+        $mover = new Mover($config);
+        $replacer = new Replacer($config);
+
+        $mover->deleteTargetDirs($packages);
+        $mover->movePackages($packages);
+        $replacer->replacePackages($packages);
+        $replacer->replaceParentInTree($packages);
+        $replacer->replaceParentClassesInDirectory($config->getClassmapDirectory());
+
+        if ($config->getDeleteVendorDirectories()) {
+            $mover->deletePackageVendorDirectories();
+        }
+    }
+}
--- src/Console/Commands/Compose.php
+++ src/Console/Commands/Compose.php
@@ -2,11 +2,7 @@
 
 namespace CoenJacobs\Mozart\Console\Commands;
 
-use CoenJacobs\Mozart\Config\Mozart;
-use CoenJacobs\Mozart\Mover;
-use CoenJacobs\Mozart\PackageFactory;
-use CoenJacobs\Mozart\PackageFinder;
-use CoenJacobs\Mozart\Replacer;
+use CoenJacobs\Mozart\Commands\Compose as ComposeCommand;
 use Exception;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
@@ -14,11 +10,6 @@ use Symfony\Component\Console\Output\OutputInterface;
 
 class Compose extends Command
 {
-    private Mover $mover;
-    private Replacer $replacer;
-    private Mozart $config;
-    private string $workingDir;
-
     protected function configure(): void
     {
         $this->setName('compose');
@@ -37,54 +28,13 @@ class Compose extends Command
             throw new Exception('Unable to determine the working directory.');
         }
 
-        $this->workingDir = $workingDir;
+        $compose = new ComposeCommand($workingDir);
 
-        $composerFile = $this->workingDir . DIRECTORY_SEPARATOR . 'composer.json';
         try {
-            $factory = new PackageFactory();
-            $package = $factory->createPackage($composerFile);
+            $compose->execute();
         } catch (Exception $e) {
-            $output->write('Unable to read the composer.json file');
+            $output->write($e->getMessage());
             return 1;
-        }
-
-        if (! $package->isValidMozartConfig() || empty($package->getExtra())) {
-            $output->write('Mozart config not readable in composer.json at extra->mozart');
-            return 1;
-        }
-
-        $config = $package->getExtra()->getMozart();
-
-        if (empty($config)) {
-            $output->write('Mozart config not readable in composer.json at extra->mozart');
-            return 1;
-        }
-
-        $this->config = $config;
-        $this->config->setWorkingDir($this->workingDir);
-
-        $require = $this->config->getPackages();
-        if (empty($require)) {
-            $require = $package->getRequire();
-        }
-
-        $finder = new PackageFinder();
-        $finder->setConfig($this->config);
-
-        $package->loadDependencies($finder);
-        $packages = $finder->findPackages($package->getDependencies());
-
-        $this->mover = new Mover($this->config);
-        $this->replacer = new Replacer($this->config);
-
-        $this->mover->deleteTargetDirs($packages);
-        $this->mover->movePackages($packages);
-        $this->replacer->replacePackages($packages);
-        $this->replacer->replaceParentInTree($packages);
-        $this->replacer->replaceParentClassesInDirectory($this->config->getClassmapDirectory());
-
-        if ($this->config->getDeleteVendorDirectories()) {
-            $this->mover->deletePackageVendorDirectories();
         }
 
         return 0;
